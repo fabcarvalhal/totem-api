12792821d2e26dd4be948add80727621bbc3e6df
Fabricio Carvalhal <fabcarvalhal@gmail.com>
2018-08-01 21:12:07 -0300

    função para validar se todos campos necessário existem

--- app/Inscricao.php
+++ app/Inscricao.php
@@ -1,10 +1,41 @@
 <?php 
 class Inscricao {
+    
+    private  $requiredKeys =  array('id_faculdade','id_curso','matricula','telefone','nome','id_evento');
 
-    public function inscrever($input) {
-        //TODO: tratar o input
-        //TODO: validar os dados
+    public function inscrever(array $input) {
+        $validateInput = $this->validateInput($input);
+        if($validateInput->error) {
+            echo json_encode($validateInput);
+            return;
+        }
+
+        
         //TODO: executar inscrição
+    }
+    
+    
+    /*
+    Checa se todas as chaves existem no input
+    Retorna um objeto com error, message
+    */
+    private function validateInput(array $input) {        
+        $response = new StdClass();
+        $response->error = null;
+        $response->message = "";
+        $missing = array_diff_key(array_flip($this->requiredKeys), $input);
+        
+        if(sizeof($missing) > 0) {
+            $response->error = true;
+            $missingKeys = array_map(function($miss){
+              return $this->requiredKeys[$miss];
+            },$missing); 
+            
+            $response->message = "Os seguintes dados estão faltando: ".implode(",",$missingKeys);
+            return $response;
+        }
+        $response->error = false;
+        return $response;
     }
 
 }
