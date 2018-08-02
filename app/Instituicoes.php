861147dd47532f8e20467ed83835665b832f0b2c
Fabricio Carvalhal <fabcarvalhal@gmail.com>
2018-08-01 22:08:39 -0300

    adcionado metodo para verificar se existe a instituicao pelo id

--- app/Instituicoes.php
+++ app/Instituicoes.php
@@ -2,17 +2,44 @@
 
 class Instituicoes {
     public static function getAll() {  
-        try{
+        try {
             $Conexao    = Conexao::getConnection();
             $query      = $Conexao->query("SELECT * FROM Instituição ");
             $instituicoes   = $query->fetchAll(PDO::FETCH_OBJ);
             echo json_encode($instituicoes); 
-        }catch(Exception $e){
+        } catch(Exception $e) {
             echo $e->getMessage();
             exit;
         }
     }
     
+
+    public function checkIfExists($id) {
+        $response = new StdClass();
+        $response->error = false;
+        $response->message = "";
+        try {
+            $conexao = Conexao::getConnection();
+            $statement = $conexao->prepare("SELECT COUNT(1) AS contagem FROM Instituição WHERE id = :id");
+            $statement->bind_param(":id", $id, PDO::PARAM_INT);
+            $statement->execute();
+            $result = $statement->fetch(PDO::FETCH_OBJ);
+
+            if($result->contagem > 0) {
+                $response->error = false;
+                return $response;
+            }
+
+            $response->error = true;
+            $response->message = "Não foi possível checar a instituição.";
+            return $response;
+        } catch(Exception $e) {
+            $response->error = true;
+            $response->message = "Não foi possível checar a instituição.";
+            return $response;
+        }
+        
+    }
 }
 
 ?>
