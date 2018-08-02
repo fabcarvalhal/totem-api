51bf4647aa91a18e2357a86a87daec9c785d2f49
Fabricio Carvalhal <fabcarvalhal@gmail.com>
2018-08-02 00:36:51 -0300

    adicionado método de inscrição nos eventos

--- app/Inscricao.php
+++ app/Inscricao.php
@@ -9,9 +9,87 @@ class Inscricao {
             echo json_encode($validateInput);
             return;
         }
-
+        $inputAsObject = (object) $input;
+        $validateInstituition = Instituicoes::checkIfExists($inputAsObject->id_faculdade);
+        if($validateInstituition->error) {
+            echo json_encode($validateInstituition);
+            return;
+        }
         
-        //TODO: executar inscrição
+        $validateCourse = Cursos::checkIfExists($inputAsObject->id_curso);
+        if($validateCourse->error) {
+            echo json_encode($validateCourse);
+            return;
+        }
+
+        $validateEvent = Eventos::checkIfExists($inputAsObject->id_evento);
+        if($validateEvent->error) {
+            echo json_encode($validateEvent);
+            return;
+        }
+
+        $getEventName = Eventos::getEventNameById($id);
+        if($getEventName->error) {
+            echo json_encode($getEventName);
+            return;
+        }
+
+        $checkHasSubscribed = Eventos::checkStudentHasSubscribed($inputAsObject->matricula, $inputAsObject->id_evento);
+        if($checkHasSubscribed->error) {
+            echo json_encode($checkHasSubscribed);
+            return;
+        }
+
+        $validateStudent = Alunos::checkIfExists($inputAsObject->matricula);
+        $student = null;
+        if($validateStudent->error == true) {
+            $student = new AlunoDTO(
+                $inputAsObject->nome,
+                $inputAsObject->matricula,
+                $inputAsObject->telefone,
+                $inputAsObject->curso,
+                $inputAsObject->faculdade,
+                $inputAsObject->email
+            );
+
+            $insertStudent = Alunos::cadastrar($student);
+            if($insertStudent->error) {
+                echo json_encode($insertStudent);
+                return;
+            }
+        }
+
+        $response = new StdClass90;
+        $response->error = false;
+        $response->message = "";
+        try {
+            $conexao = Conexao::getConnection();
+            $statement = $conexao->prepare("INSERT INTO :tabela (aluno,nome_aluno,checkin,checkout) VALUES(:matricula,:nomealuno,0,0)");
+            $statement->bindValue(":tabela", $getEventName, PDO::PARAM_STR);
+            $statement->bindValue(":matricula", $student->matricula, PDO::PARAM_INT);
+            $statement->bindValue(":nomealuno", $student->nome, PDO::PARAM_STR);
+            $statement->execute();
+
+            if($statement->rowCount() > 0) {
+                $response->message = "Cadastro realizado com sucesso!";
+                echo json_encode($response);
+                return;
+            }
+
+            $response->error = true;
+            $response->message = "Não foi possível cadastrar, tente novamente mais tarde.";
+            echo json_encode($response);
+            return;
+
+        } catch(Exception $e) {
+            $response->error = true;
+            $response->message = "Erro ao cadastrar, tente novamente mais tarde.";
+            echo json_encode($response);
+            return;
+        }
+
+
+
     }
     
     
