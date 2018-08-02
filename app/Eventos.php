5e3c579d91770624fab12e81af855c60fcd5cdc2
Fabricio Carvalhal <fabcarvalhal@gmail.com>
2018-08-01 22:47:17 -0300

    adicionada função para verificar se evento existe

--- app/Eventos.php
+++ app/Eventos.php
@@ -13,6 +13,33 @@ class Eventos {
             exit;
         }
     }
+
+    public function checkIfExists($id) {
+        $response = new StdClass();
+        $response->error = false;
+        $response->message = "";
+        try {
+            $conexao = Conexao::getConnection();
+            $statement = $conexao->prepare("SELECT COUNT(1) AS contagem FROM Eventos WHERE id = :id");
+            $statement->bind_param(":id", $id, PDO::PARAM_INT);
+            $statement->execute();
+            $result = $statement->fetch(PDO::FETCH_OBJ);
+
+            if($result->contagem > 0) {
+                $response->error = false;
+                return $response;
+            }
+
+            $response->error = true;
+            $response->message = "Não foi possível checar o evento.";
+            return $response;
+        } catch(Exception $e) {
+            $response->error = true;
+            $response->message = "Não foi possível checar o evento.";
+            return $response;
+        }
+        
+    }
 }
 
 ?>
